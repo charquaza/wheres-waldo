4d6d4a9883d7ce5336b68aac6366d434aaa857a7
Jonathan Park <johanpk10@gmail.com>
2021-07-18 10:08:00 -0700

    Add firebase

---
+++ firebaseConfig.example.js
@@ -0,0 +1,5 @@
+// TODO: Replace the following with your app's Firebase project configuration
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+const firebaseConfig = {
+    // ...
+};
