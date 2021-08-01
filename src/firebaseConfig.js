41e4d35f004d0690e42b18f87e08fcdfbaac75ff
Jonathan Park <johanpk10@gmail.com>
2021-07-31 17:59:00 -0700

    Add firebase config file

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA7lyPyI42XBZIvt4c05DVfw87Tu-4IqZU",
+    authDomain: "wheres-waldo-odin-jp.firebaseapp.com",
+    projectId: "wheres-waldo-odin-jp",
+    storageBucket: "wheres-waldo-odin-jp.appspot.com",
+    messagingSenderId: "179199665105",
+    appId: "1:179199665105:web:6e232fa769b6e39befb6f0"
+};
+
+export default firebaseConfig;
